21e6db9bcc250e3a1fe711a8be582ea84eaa5efb
abdurrahman <a.r.moazzam6171@gmail.com>
2025-10-20 22:16:31 +0500

    index page

---
+++ products.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    id: 1,
+    name: "YSL Black Bag",
+    price: "PKR 2450/-",
+    image: "content/bags/DSC00169.webp",
+    images: [
+      "content/bags/DSC00169.webp",
+      "content/bags/DSC00170.webp",
+      "content/bags/DSC00171.webp",
+    ],
+    shortDescription:
+      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+    description:
+      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+    inStock: 1,
+  },
+];
