5c0f4845773271ceaf7b2874c4ba0ff91534b47d
abdurrahman <a.r.moazzam6171@gmail.com>
2025-10-24 17:52:35 +0500

    hero, products, page linking

--- products.js
+++ products.js
@@ -15,4 +15,36 @@ const products = [
       "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
     inStock: 1,
   },
+  {
+    id: 2,
+    name: "YSL Black Bag",
+    price: "PKR 2450/-",
+    image: "content/bags/DSC00169.webp",
+    images: [
+      "content/bags/DSC00169.webp",
+      "content/bags/DSC00170.webp",
+      "content/bags/DSC00171.webp",
+    ],
+    shortDescription:
+      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+    description:
+      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+    inStock: 1,
+  },
+  {
+    id: 3,
+    name: "YSL Black Bag",
+    price: "PKR 2450/-",
+    image: "content/bags/DSC00169.webp",
+    images: [
+      "content/bags/DSC00169.webp",
+      "content/bags/DSC00170.webp",
+      "content/bags/DSC00171.webp",
+    ],
+    shortDescription:
+      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+    description:
+      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+    inStock: 1,
+  },
 ];
