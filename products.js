520180bff7350e8cf2c487fa176f09a388793046
abdurrahman <a.r.moazzam6171@gmail.com>
2025-12-30 23:18:54 +0500

    products

--- products.js
+++ products.js
@@ -3,11 +3,11 @@ const products = [
     id: 1,
     name: "YSL Black Bag",
     price: "PKR 2450/-",
-    image: "content/bags/DSC00169.webp",
+    image: "content/IMG-20251024-WA0018 (1).jpg",
     images: [
-      "content/bags/DSC00169.webp",
-      "content/bags/DSC00170.webp",
-      "content/bags/DSC00171.webp",
+      "content/IMG-20251024-WA0019 (1).jpg",
+      "content/IMG-20251024-WA0020 (1).jpg",
+      "content/IMG-20251024-WA0018 (1).jpg",
     ],
     shortDescription:
       "Black YSL Bag, deep textured, perfect for daily and occasional use.",
@@ -19,11 +19,28 @@ const products = [
     id: 2,
     name: "YSL Black Bag",
     price: "PKR 2450/-",
-    image: "content/bags/DSC00169.webp",
+    image: "content/IMG-20251024-WA0040 (1).jpg",
     images: [
-      "content/bags/DSC00169.webp",
-      "content/bags/DSC00170.webp",
-      "content/bags/DSC00171.webp",
+      "content/IMG-20251024-WA0040 (1).jpg",
+      "content/IMG-20251024-WA0041 (1).jpg",
+      "content/IMG-20251024-WA0043 (1).jpg",
+      "content/IMG-20251024-WA0044 (1).jpg",
+    ],
+    shortDescription:
+      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+    description:
+      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+    inStock: 0,
+  },
+  {
+    id: 3,
+    name: "YSL Black Bag",
+    price: "PKR 2450/-",
+    image: "content/IMG-20251024-WA0031 (1).jpg",
+    images: [
+      "content/IMG-20251024-WA0024 (1).jpg",
+      "content/IMG-20251024-WA0027 (1).jpg",
+      "content/IMG-20251024-WA0025 (1).jpg",
     ],
     shortDescription:
       "Black YSL Bag, deep textured, perfect for daily and occasional use.",
@@ -32,14 +49,30 @@ const products = [
     inStock: 1,
   },
   {
-    id: 3,
+    id: 4,
     name: "YSL Black Bag",
     price: "PKR 2450/-",
-    image: "content/bags/DSC00169.webp",
+    image: "content/IMG-20251024-WA0023 (1).jpg",
     images: [
-      "content/bags/DSC00169.webp",
-      "content/bags/DSC00170.webp",
-      "content/bags/DSC00171.webp",
+      "content/IMG-20251024-WA0023 (1).jpg",
+      "content/IMG-20251024-WA0022 (1).jpg",
+      "content/IMG-20251024-WA0021 (1).jpg",
+    ],
+    shortDescription:
+      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+    description:
+      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+    inStock: 1,
+  },
+  {
+    id: 5,
+    name: "YSL Black Bag",
+    price: "PKR 2450/-",
+    image: "content/IMG-20251024-WA0038 (1).jpg",
+    images: [
+      "content/IMG-20251024-WA0038 (1).jpg",
+      "content/IMG-20251024-WA0039 (1).jpg",
+      "content/IMG-20251024-WA0037 (1).jpg",
     ],
     shortDescription:
       "Black YSL Bag, deep textured, perfect for daily and occasional use.",
