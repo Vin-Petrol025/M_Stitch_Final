a88152df784b2adbe6cb942701b1b39ed82dc8aa
abdurrahman <a.r.moazzam6171@gmail.com>
2026-04-13 21:00:24 +0500

    media-encoded

--- products.js
+++ products.js
@@ -1,8 +1,8 @@
 const products = [
   {
     id: 1,
-    name: "YSL Black Bag",
-    price: "PKR 2450/-",
+    name: "Solid Maroon",
+    price: "PKR 13,500/-",
     image: "content/IMG-20251024-WA0018 (1).jpg",
     images: [
       "content/IMG-20251024-WA0019 (1).jpg",
@@ -10,33 +10,33 @@ const products = [
       "content/IMG-20251024-WA0018 (1).jpg",
     ],
     shortDescription:
-      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+      "korean silk shirt, trousers and dupatta with hand work of stones and sequence on shirt. Organza lace on dupatta.",
     description:
-      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+      "Korean raw silk for shirt and trousers, silk dupatta, hand work of stones and sequence on shirt. Organza lace on dupatta. Stitched 3-piece. Availabke in any customizable size.",
     inStock: 1,
     sizeChartImage: "content/IMG-20251024-WA0018 (1).jpg",
   },
   {
     id: 2,
-    name: "YSL Black Bag",
-    price: "PKR 2450/-",
+    name: "Mustard Shirt with Shalwar.",
+    price: "PKR 3,250/-",
     image: "content/IMG-20251024-WA0040 (1).jpg",
     images: [
-      "content/IMG-20251024-WA0040 (1).jpg",
+      "content/VID-20260413-WA0067.mp4",
       "content/IMG-20251024-WA0041 (1).jpg",
       "content/IMG-20251024-WA0043 (1).jpg",
       "content/IMG-20251024-WA0044 (1).jpg",
     ],
     shortDescription:
-      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+      "Mustard colored shirt with matching shalwar and dupatta.",
     description:
-      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+      "Tailor Stitched 2 piece outfit, Premium Lawn for shirt and self print cotton for shalwar. Sequence on necklace. Available in any customizable size.",
     inStock: 1,
   },
   {
     id: 3,
-    name: "YSL Black Bag",
-    price: "PKR 2450/-",
+    name: "Garden Green",
+    price: "PKR 3,950/-",
     image: "content/IMG-20251024-WA0031 (1).jpg",
     images: [
       "content/IMG-20251024-WA0024 (1).jpg",
@@ -44,31 +44,16 @@ const products = [
       "content/IMG-20251024-WA0025 (1).jpg",
     ],
     shortDescription:
-      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+      " Lawn Shirt with trouser and dupatta.",
     description:
-      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+      "Tailor stitched 2 piece outfir. Premium lawn shirt and trousers. Milky lace on hem and sleeves. Available in any customizable size.",
     inStock: 1,
   },
+
   {
     id: 4,
-    name: "YSL Black Bag",
-    price: "PKR 2450/-",
-    image: "content/IMG-20251024-WA0023 (1).jpg",
-    images: [
-      "content/IMG-20251024-WA0023 (1).jpg",
-      "content/IMG-20251024-WA0022 (1).jpg",
-      "content/IMG-20251024-WA0021 (1).jpg",
-    ],
-    shortDescription:
-      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
-    description:
-      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
-    inStock: 1,
-  },
-  {
-    id: 5,
-    name: "YSL Black Bag",
-    price: "PKR 2450/-",
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
     image: "content/IMG-20251024-WA0038 (1).jpg",
     images: [
       "content/IMG-20251024-WA0038 (1).jpg",
@@ -76,9 +61,218 @@ const products = [
       "content/IMG-20251024-WA0037 (1).jpg",
     ],
     shortDescription:
-      "Black YSL Bag, deep textured, perfect for daily and occasional use.",
+      "Peach colored shirt with matching shalwar.",
     description:
-      "Black Colored YSL Bag, Deep textured surface, Perfect for daily and occasional use. Spacious interior, premium hardware, and a timeless design make it suitable for both casual and formal outings.",
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 5,
+    name: "Lailac chicken Kari",
+    price: "PKR 9,800/-",
+    image: "content/IMG-20260413-WA0053.jpg",
+    
+    images: [
+"content/IMG-20260413-WA0052.jpg", 
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0054.jpg",
+"content/IMG-20260413-WA0047.mp4",
+"content/IMG-20260413-WA0044.mp4",
+"content/IMG-20260413-WA0042.mp4",
+"content/IMG-20260413-WA0041.mp4",
+    
+        ],
+    shortDescription:
+    "Chicken kari summer collection",
+    description:
+      "chicken kari shirt summer cotton trousers(breathable fabric) Lawn embroidred dupatta with organza border embroided patch work on sleeves and backside of shirt. tassels also attached, u can add on a silk or chifoon gupattra. Available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 6,
+    name: "White Rose",
+    price: "PKR 35,000/-",
+    image: "content/IMG-20260413-WA0040.jpg",
+
+    images: [
+"content/IMG-20260413-WA0040.jpg",
+"content/IMG-20260413-WA0037.jpg",
+"content/IMG-20260413-WA0043.jpg",
+"content/IMG-20260413-WA0032.mp4",
+"content/IMG-20260413-WA0031.mp4",
+"content/IMG-20260413-WA0031.mp4",
+"content/IMG-20260413-WA0039.mp4",
+"content/IMG-20260413-WA0038.mp4",
+"content/IMG-20260413-WA0036.mp4",
+"content/IMG-20260413-WA0035.mp4",
+"content/IMG-20260413-WA0034.mp4",
+"content/IMG-20260413-WA0082.jpg",
+        ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 7,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+        ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 8,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+ 
+        ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 9,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 10,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 11,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 12,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 13,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 14,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 15,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
+    inStock: 1,
+  },
+   {
+    id: 16,
+    name: "peach shirt with shalwar",
+    price: "PKR 3,950/-",
+   image: "content/IMG-20260413-WA0053.jpg",
+        images: [
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+"content/IMG-20260413-WA0053.jpg",
+     ],
+    shortDescription:
+      "Peach colored shirt with matching shalwar.",
+    description:
+      "Tailor stitched 2 piece outfir. Premium lawn for shirt, summer collection with farshi shalwar, milky lace and pearl tassels. available in any customizable size.",
     inStock: 1,
   },
 ];
